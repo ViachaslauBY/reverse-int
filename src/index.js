872353b5070f27d51daf15191f235d68e658cda3
Вячеслав Федорцов <viachaslaufedartsou@gmail.com>
2020-10-05 16:42:00 +0300

    feat: copmlete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let num = n.toString().split('').reverse().join('');    
+   
+   if(num[num.length - 1] === '-') {
+     return num.slice(0, -1);
+   } else {
+     return num;
+   }
 }
